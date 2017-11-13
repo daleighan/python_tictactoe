72ed45601071efd8b6972f72f3e7b3f1f777c111
David Leigh <daleighan@gmail.com>
2017-11-12 23:37:51 -0800

    Started making python tictactoe

---
+++ tictactoe.py
@@ -0,0 +1,53 @@
+class Game:
+    def __init__(self):
+        self.player1 = input("Who is player 1?")
+        self.player2 = input("Who is player 2?")
+        self.won = False
+        self.move_count = 0
+        self.board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+        self.print_board()
+        while self.won == False:
+            print(self.player1, "moves")
+            row = int(input("row?"))
+            column = int(input("column"))
+            self.toggle_place("X", row, column)
+            self.check_wins("X", self.player1)
+            if self.won == True:
+                break
+            self.print_board()
+            print(self.player2, "moves")
+            row = int(input("row?"))
+            column = int(input("column"))
+            self.toggle_place("O", row, column)
+            self.check_wins("O", self.player2)
+            self.print_board()
+
+    def print_board(self):
+        print(self.board[0])
+        print(self.board[1])
+        print(self.board[2])
+
+    def toggle_place(self, piece,  i, j):
+        self.board[i][j] = piece
+
+    def check_wins(self, piece, player):
+        if self.board[0][0] == piece and self.board[0][1] == piece and self.board[0][2] == piece:
+            self.won = True        
+        if self.board[1][0] == piece and self.board[1][1] == piece and self.board[1][2] == piece:
+            self.won = True
+        if self.board[2][0] == piece and self.board[2][1] == piece and self.board[2][2] == piece:
+            self.won = True
+        if self.board[0][0] == piece and self.board[1][0] == piece and self.board[2][0] == piece:
+            self.won = True
+        if self.board[0][1] == piece and self.board[1][1] == piece and self.board[2][1] == piece:
+            self.won = True
+        if self.board[0][2] == piece and self.board[1][2] == piece and self.board[2][2] == piece:
+            self.won = True
+        if self.board[0][0] == piece and self.board[1][1] == piece and self.board[2][2] == piece:
+            self.won = True
+        if self.board[0][2] == piece and self.board[1][1] == piece and self.board[2][0] == piece:
+            self.won = True
+        if self.won == True:
+            print(player, "wins")
+
+new_game = Game()
